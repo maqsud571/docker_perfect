ddaa57cb170812123ad3100de2a96ed34cb62e9e
Maqsud571 <uzbpubgmm24@gmail.com>
2023-12-26 18:53:06 +0500

    second chenjs

--- User/models.py
+++ User/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class UserModel(models.Model):
-    name = models.CharField(max_length=60)
+    name = models.CharField(max_length=60,unique = True)
     password = models.IntegerField()
 
     def __str__(self):
--- django.sh
+++ django.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+# ==========================
 echo "Create migrations"
 python manage.py makemigrations
 echo "====================================="
